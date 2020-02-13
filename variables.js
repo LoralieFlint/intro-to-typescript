cf10214b56a45ed04d997c19bbdd9713d2fd8c09
ittyBittyBytes <loralie.flint0128@gmail.com>
2020-02-13 16:12:39 -0700

    ran my first typescript compile to JS

---
+++ variables.js
@@ -0,0 +1,20 @@
+// variables and compiling
+var bob = "Hey im Bob";
+console.log(bob);
+var a = 5.312;
+// same as
+var ab = 5.55;
+var obj = {
+    x: 5.32,
+    y: 7
+};
+// these wont work
+// obj.x = "123"
+// obj.y = true
+// to compile run tsc variables.ts
+// this will compile into JS and  make a JS file in the same name
+// functions
+// unions
+// casting
+// any
+//
--- variables.ts
+++ variables.ts
@@ -2,22 +2,22 @@
 const bob = "Hey im Bob";
 console.log(bob);
 
-const a = 5.312
-// same as 
-const ab: number = 5.55
+const a = 5.312;
+// same as
+const ab = 5.55;
 
 // creating an interface for it to check for value types
 interface myObj {
-    x: number,
-    y: number,
-    // adding an optional property with ?
-    z?: number
+  x: number;
+  y: number;
+  // adding an optional property with ?
+  z?: number;
 }
 
 const obj: myObj = {
-    x: 5.32,
-    y: 7
-}
+  x: 5.32,
+  y: 7
+};
 
 // these wont work
 // obj.x = "123"
@@ -26,14 +26,11 @@ const obj: myObj = {
 // to compile run tsc variables.ts
 // this will compile into JS and  make a JS file in the same name
 
-
-
-
-// functions 
+// functions
 
 // unions
 
-// casting 
+// casting
 
 // any
 
