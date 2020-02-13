936bff40fbc38f76f37104ad98ac4bfe11c865cd
ittyBittyBytes <loralie.flint0128@gmail.com>
2020-02-13 16:52:38 -0700

    2nd compile

--- variables.js
+++ variables.js
@@ -9,11 +9,20 @@ var obj = {
     y: 7
 };
 // functions
-var add = function (x, y) { return x + y; };
-console.log(add(2, 3));
+var add1 = function (x, y) { return x + y; };
+console.log(add1(2, 3));
 // destructuring 
 var add2 = function (nums) { return nums.a + nums.b; };
 // unions
+var maybeNumber = 5;
+maybeNumber = "Hello.";
+var dogCat = {
+    bark: "bark",
+    purr: "purr"
+};
+if (typeof dogCat === "string") {
+    console.log("this is a string");
+}
 // casting
 // any
 //
